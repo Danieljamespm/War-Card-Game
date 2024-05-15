bff0855018046ec97490fb14d3889a34a1ec0dba
Danny Mettler <danieljamespm@gmail.com>
2024-05-14 23:52:47 -0400

    trying to add a way to determine the winner.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,9 @@ let deckId = ''
 document.querySelector('#war').classList.add('hidden')
 
 
+
+
+
 fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
       .then(res => res.json()) // parse response as JSON
       .then(data => {
